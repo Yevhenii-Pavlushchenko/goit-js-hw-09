b7644fbfb4de3e738eb3a643e7dfd4ad5fad4bdc
Yevhenii-Pavlushchenko <103812722+Yevhenii-Pavlushchenko@users.noreply.github.com>
2026-02-12 12:32:17 +0000

    Deploying to gh-pages from @ Yevhenii-Pavlushchenko/goit-js-hw-09@8c8f39ae116be46f22c73b9cc17162c9990f1af7 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Hello this is Form");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const e=document.querySelector(".feedback-form");e.addEventListener("input",o);e.addEventListener("submit",n);function n(t){t.preventDefault()}function o(t){}
 //# sourceMappingURL=2-form.js.map
